24f24bf5a8e9529e37ef35b33c02831cb8ccb3e8
Alberson Miranda <albersonmiranda@hotmail.com>
2022-06-20 10:39:30 -0300

    add candidatos 2022 and configs

--- render.R
+++ render.R
@@ -1,10 +1,13 @@
-candidatos = readxl::read_excel("candidatos/candidatos.xlsx")
+# parametros
+candidatos = readxl::read_excel("candidatos/candidatos.xlsx") |>
+  subset(fase1 == TRUE)
 prova = 2022
 
+# renderização
 render_one = function(prova, candidato) {
   rmarkdown::render(
     paste0(prova, ".Rmd"),
-    output_file = paste0("provas/", candidato, ".pdf"),
+    output_file = paste0("provas/", gsub(" ", "_", candidato), ".pdf"),
     params = list(
         candidato = candidato),
     envir = parent.frame()
